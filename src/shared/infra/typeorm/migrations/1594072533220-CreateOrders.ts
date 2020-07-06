98040cd8c496f3221693db2314c3ab1d803a17df
Diego Veiga Santos <sdiegoveiga@gmail.com>
2020-07-06 18:55:52 -0300

    create products

---
+++ src/shared/infra/typeorm/migrations/1594072533220-CreateOrders.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateOrders1594072533220 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
